3c733f78fcaa0cf83946f5af6a208ed49e435764
jonghyeon <whdgus003@gmail.com>
2023-05-11 17:33:58 +0900

    init: fix args to string

--- src/main/java/com/jbyee/Main.java
+++ src/main/java/com/jbyee/Main.java
@@ -6,7 +6,7 @@ import com.theokanning.openai.service.OpenAiService;
 public class Main {
     public static void main(String[] args) {
         String function = "function exampleFunction(a: number, b: number): number {\n  return a + b;\n}";
-        Object[] arguments = {1,2};
+        Object[] arguments = {Integer.valueOf(1),Integer.valueOf(2)};
         String description = "Adds two numbers together";
         String model = "gpt-3.5-turbo";
         AISupporter aiSupporter = new AISupporter(new OpenAiService("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"));
--- src/main/java/com/jbyee/ai/AISupporter.java
+++ src/main/java/com/jbyee/ai/AISupporter.java
@@ -9,7 +9,9 @@ import com.theokanning.openai.service.OpenAiService;
 import lombok.RequiredArgsConstructor;
 
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class AISupporter {
@@ -18,8 +20,9 @@ public class AISupporter {
 
 
     public <T> T aiFunction(String function, Class<T> returnType, Object[] args, String description, String model) {
+
         // parse args to comma separated string
-        String argsString = String.join(", ", args.toString());
+        String argsString = String.join(", ", Arrays.stream(args).map(Object::toString).collect(Collectors.joining(", ")));
         List<ChatMessage> messages = List.of(
                 new ChatMessage(Role.SYSTEM.getValue(), "You are now the following TypeScript function: ```# " + description + "\n" + function + "```\n\nOnly respond with your `return` value. Do not include any other explanatory text in your response."),
                 new ChatMessage(Role.USER.getValue(), argsString)
@@ -30,9 +33,13 @@ public class AISupporter {
                 .messages(messages)
                 .build());
 
+//        Arrays.stream(returnType.getFields()).map(field -> field.getType())
+
         // Use ObjectMapper to convert the response to the specified return type
         String content = response.getChoices().get(0).getMessage().getContent();
         ObjectMapper objectMapper = new ObjectMapper();
+
+
         T result = objectMapper.convertValue(content, returnType);
         return result;
     }
