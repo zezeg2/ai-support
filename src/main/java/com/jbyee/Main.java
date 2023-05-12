8da38c7933701c41fb45897445786182f1dbd046
jonghyeon <whdgus003@gmail.com>
2023-05-12 11:24:04 +0900

    refactor: add default result type as Map

--- src/main/java/com/jbyee/Main.java
+++ src/main/java/com/jbyee/Main.java
@@ -1,5 +1,6 @@
 package com.jbyee;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.jbyee.ai.AISupporter;
 import com.jbyee.ai.ArgumentRecord;
 import com.jbyee.ai.ArgumentsFactory;
@@ -7,19 +8,21 @@ import com.jbyee.resolver.JAVAConstructResolver;
 import com.theokanning.openai.service.OpenAiService;
 
 import java.util.List;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
-        String functionName = "generateDocument";
+    public static void main(String[] args) throws JsonProcessingException {
+        String functionName = "generateProsAndConsMap";
         List<ArgumentRecord> records = ArgumentsFactory
                 .builder()
                 .addArgument("pros", 4, Integer.class)
                 .addArgument("cons", 4, Integer.class)
+                .addArgument("docType", "markDown", String.class)
                 .build();
-        String description = "generate Document about Python, list pros and cons in given number";
+        String description = "this function generate 'pros' and 'cons' in given number about Python.  and store them in Map. keys are 'pros' , 'cons'value is list";
         String model = "gpt-3.5-turbo";
         AISupporter aiSupporter = new AISupporter(new OpenAiService("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"), new JAVAConstructResolver());
-        String result = aiSupporter.<String>aiFunction(functionName, String.class, records, description, model);
+        Map<String, Object> result = aiSupporter.aiFunction(functionName, records, description, model);
         System.out.println(result);
 
     }
--- src/main/java/com/jbyee/ai/AISupporter.java
+++ src/main/java/com/jbyee/ai/AISupporter.java
@@ -1,5 +1,6 @@
 package com.jbyee.ai;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jbyee.common.enums.ROLE;
 import com.jbyee.resolver.ConstructResolver;
@@ -11,7 +12,9 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -24,13 +27,29 @@ public class AISupporter {
     private ConstructResolver resolver;
 
 
-    public <T> T aiFunction(String functionName, Class<T> returnType, List<ArgumentRecord> args, String description, String model) {
-        // parse args to comma separated string
+    public Map<String, Object> aiFunction(String functionName, List<ArgumentRecord> args, String description, String model) throws JsonProcessingException {
+        String function = createFunction(null, functionName, args);
+        String refTypes = resolveRefTypes(args);
+        List<ChatMessage> messages = createChatMessages(description, refTypes, functionName, function, args);
+        ChatCompletionResult response = executeChatCompletion(model, messages);
+        return parseResponse(response, LinkedHashMap.class);
+    }
 
-        String filedsString = args.stream().map(ArgumentRecord::getField).collect(Collectors.joining(", "));
-        String valuesString = args.stream().map(ArgumentRecord::getValue).collect(Collectors.joining(", "));
-        String filedTypesString = args.stream().map(argumentRecord -> argumentRecord.getType() + " " + argumentRecord.getField()).collect(Collectors.joining(", "));
-        String function = """
+    public <T> T aiFunction(String functionName, Class<T> returnType, List<ArgumentRecord> args, String description, String model) throws JsonProcessingException {
+        String function = createFunction(returnType, functionName, args);
+        String refTypes = resolveRefTypes(args);
+        List<ChatMessage> messages = createChatMessages(description, refTypes, functionName, function, args);
+        ChatCompletionResult response = executeChatCompletion(model, messages);
+        return parseResponse(response, returnType);
+    }
+
+    private <T> String createFunction(Class<T> returnType, String functionName, List<ArgumentRecord> args) {
+        String fieldsString = args.stream().map(ArgumentRecord::getField).collect(Collectors.joining(", "));
+        String fieldTypesString = args.stream()
+                .map(argumentRecord -> argumentRecord.getType() + " " + argumentRecord.getField())
+                .collect(Collectors.joining(", "));
+
+        return """
                 @FunctionalInterface
                 public interface FC {
                     %s %s(%s);
@@ -39,15 +58,22 @@ public class AISupporter {
                     public static void main(String[] args) {
                         FC fc = (%s) -> {
                             //TODO: implement or just return result
-                            return //TODO: fill your result here
+                            return //TODO: fill your result here. your return will be converted by ObjectMapper.
                         };
                     }
                 }
-                
-                """.formatted(returnType.getSimpleName(), functionName, filedTypesString,  filedsString);
-        Set<? extends Class<?>> classList = args.stream().map(ArgumentRecord::type).collect(Collectors.toSet());
-        String refTypes = resolver.resolve((Set<Class<?>>) classList);
-        List<ChatMessage> messages = List.of(
+                """.formatted(returnType != null ? returnType.getSimpleName() : "Map<String, Object>", functionName, fieldTypesString, fieldsString);
+    }
+
+    private String resolveRefTypes(List<ArgumentRecord> args) {
+        Set<Class<?>> classList = args.stream().map(ArgumentRecord::type).collect(Collectors.toSet());
+        return resolver.resolve(classList);
+    }
+
+    private List<ChatMessage> createChatMessages(String description, String refTypes, String functionName, String function, List<ArgumentRecord> args) {
+        String valuesString = args.stream().map(ArgumentRecord::getValue).collect(Collectors.joining(", "));
+
+        return List.of(
                 new ChatMessage(ROLE.SYSTEM.getValue(), "You are now the following Java Lambda function: "
                         + "```java \n"
                         + description + "\n"
@@ -58,16 +84,18 @@ public class AISupporter {
                         "Only respond with your `return` value. Do not include any other explanatory text in your response."),
                 new ChatMessage(ROLE.USER.getValue(), valuesString)
         );
+    }
 
-        ChatCompletionResult response = service.createChatCompletion(ChatCompletionRequest.builder()
+    private ChatCompletionResult executeChatCompletion(String model, List<ChatMessage> messages) {
+        return service.createChatCompletion(ChatCompletionRequest.builder()
                 .model(model)
                 .messages(messages)
                 .build());
+    }
 
-        // Use ObjectMapper to convert the response to the specified return type
+    private <T> T parseResponse(ChatCompletionResult response, Class<T> returnType) throws JsonProcessingException {
         String content = response.getChoices().get(0).getMessage().getContent();
-        ObjectMapper objectMapper = new ObjectMapper();
-        T result = objectMapper.convertValue(content, returnType);
-        return result;
+        ObjectMapper mapper = new ObjectMapper();
+        return !returnType.getSimpleName().equals("LinkedHashMap") ? mapper.convertValue(content, returnType) : mapper.readValue(content, returnType);
     }
 }
