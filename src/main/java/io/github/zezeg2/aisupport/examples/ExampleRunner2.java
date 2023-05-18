1efe302a94bfb53e8a9eb35d3b3a7b7a64c41565
jonghyeon <whdgus003@gmail.com>
2023-05-18 10:48:20 +0900

    fix: edit .gitignore to exclude examples package

--- src/main/java/io/github/zezeg2/aisupport/examples/ExampleRunner2.java
+++
@@ -1,9 +0,0 @@
-package io.github.zezeg2.aisupport.examples;
-
-public class ExampleRunner2 {
-    public static void main(String[] args) throws IllegalAccessException {
-        AnalysisPromptResponse analysisPromptResponse = new AnalysisPromptResponse();
-        String example = analysisPromptResponse.getExample();
-        System.out.println(example);
-    }
-}
