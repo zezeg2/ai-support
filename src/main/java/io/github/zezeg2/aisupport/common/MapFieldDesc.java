79f60f2dc6024f983d351d05f7a3340ed5e765aa
jonghyeon <whdgus003@gmail.com>
2023-05-23 01:05:03 +0900

    feat: Supportable buildMap cover Map type

---
+++ src/main/java/io/github/zezeg2/aisupport/common/MapFieldDesc.java
@@ -0,0 +1,10 @@
+package io.github.zezeg2.aisupport.common;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MapFieldDesc {
+    String key() default "";
+    String value() default "";
+}
